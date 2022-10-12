fbd0eefc89749b46489a88f204afbc58cf929b1a
Yuvaraj Chesetti <chesetti@cs.utah.edu>
2022-10-12 16:34:59 -0700

    Generating a index.html for nightly runs

--- Makefile
+++ Makefile
@@ -1,5 +1,7 @@
 BUILD_DIR :=./out/Nightly
-NIGHTLY_OUT_DIR:=./skia_opt_research/out/$(shell date +'%Y-%m-%d_%H-%M-%S')
+NIGHTLY_REPORT_DIR:=./skia_opt_research/out/$(shell date +'%Y-%m-%d_%H-%M-%S')
+REPORT_GENERATOR=python3 ./skia_opt_research/gen_report.py
+REPORT_TEMPLATE=./skia_opt_research/report_template.html
 SKPS := $(shell ls -d ./skia_opt_research/skps/*)
 
 clean:
@@ -13,14 +15,15 @@ build-nightly: gen-nightly
 	ninja -C $(BUILD_DIR) skp_opt_membench
 
 nightly-dry:
-	@@echo mkdir -p $(NIGHTLY_OUT_DIR)
+	@@echo mkdir -p $(NIGHTLY_REPORT_DIR)
 	@@echo $(BUILD_DIR)/skp_opt_membench --skps $(SKPS) --out_dir $(NIGHTLY_OUT_DIR)
-	@@echo scp -r -C $(NIGHTLY_OUT_DIR) uwplse.org:/var/www/skia
+	@@echo scp -r -C $(NIGHTLY_REPORT_DIR) uwplse.org:/var/www/skia
 
 list-skps:
 	@@echo "List of SKPS: " $(SKPS)
 
 nightly: list-skps clean build-nightly
-	mkdir -p $(NIGHTLY_OUT_DIR)
-	$(BUILD_DIR)/skp_opt_membench --skps $(SKPS) --out_dir $(NIGHTLY_OUT_DIR)
-	scp -r -C $(NIGHTLY_OUT_DIR) uwplse.org:/var/www/skia/
+	mkdir -p $(NIGHTLY_REPORT_DIR)
+	$(BUILD_DIR)/skp_opt_membench --skps $(SKPS) --out_dir $(NIGHTLY_REPORT_DIR)
+	$(REPORT_GENERATOR) -d $(NIGHTLY_REPORT_DIR) -t $(REPORT_TEMPLATE)
+	scp -r -C $(NIGHTLY_REPORT_DIR) uwplse.org:/var/www/skia/
---
+++ skia_opt_research/gen_report.py
@@ -0,0 +1,55 @@
+import sys
+import csv
+import os
+import jinja2
+import argparse
+
+'''
+This script assumes that skp_opt_membench will dump the following files in a directory
+1. output a 000_summary_csv.txt
+2. For each skp in the above file, a <skp_name>_<optimization>_log.txt file which contains memory allocations per draw call.
+
+The script will write an index.html in the report directory.
+
+Usage
+python gen_report.py -d <dir_from_membench>
+'''
+
+parser = argparse.ArgumentParser(description='Generate a HTML report of skp_opt_membench results.')
+parser.add_argument('-d', '--report_dir', help='directory containing results of a skp_opt_membench run')
+parser.add_argument('-t', '--report_template', default='report_template.html', help='path to the html template')
+
+CSV_SUMMARY_FILE_NAME = "000_summary_csv.txt"
+
+args = parser.parse_args()
+
+membench_summary_filepath = os.path.join(args.report_dir, CSV_SUMMARY_FILE_NAME);
+report_template_filepath = os.path.abspath(args.report_template)
+
+with open(membench_summary_filepath) as csvfile:
+    results_csv = csv.DictReader(csvfile)
+    skp_name_field = results_csv.fieldnames[0]
+    opts = results_csv.fieldnames[1:]
+
+    skp_membench_results = []
+    for result_csv_row in results_csv:
+        skp_membench_result = {}
+        skp_membench_result['name'] = os.path.basename(result_csv_row[skp_name_field])
+        for opt in opts:
+            skp_membench_result[opt] = {}
+            skp_membench_result[opt]['value'] = result_csv_row[opt]
+            skp_membench_result[opt]['link'] = ('./%s_%s_log.txt' % (skp_membench_result['name'], opt)) 
+        skp_membench_results.append(skp_membench_result)
+
+    template_loader = jinja2.FileSystemLoader(searchpath = "/")
+    template_env = jinja2.Environment( loader=template_loader)
+
+    template = template_env.get_template(report_template_filepath)
+    template_vars = {
+        "title": os.path.basename(args.report_dir),
+        "skp_membench_opts": opts,
+        "skp_membench_results": skp_membench_results
+    }
+
+    with open(os.path.join(args.report_dir, "index.html"), "w") as f:
+        f.write(template.render(template_vars))
