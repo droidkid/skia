7cd817289be7e6489848baa1e60fcc89a981bd99
droidkid <yuvaraj.chesetti@gmail.com>
2022-10-24 12:31:24 -0600

    Adding instructions and tools to add new Skps

---
+++ skia_opt_research/README.md
@@ -0,0 +1,23 @@
+## Running the benchmark
+
+From the skia root directory, assuming the build directory is `out/Debug`
+Make sure you've checkout `SkiaOpt`
+
+```bash
+$ make nightly
+```
+
+This will generate a directory with HTML report in `skia_opt_research/out/<YYYY-MM-DD-mm-hh-ss>`
+
+## Adding a new Skp
+
+Add a new draw test_case to `gen_skp.cpp`.
+
+Add a raster call to the main function in `gen_skp.cpp`.
+
+From the skia root directory, assuming the build directory is `out/Debug`
+
+```bash
+$ ninja -C ./out/Debug skp_opt_gen_skps
+$ ./out/Debug skp_opt_gen_skps
+```
---
+++ skia_opt_research/gen_skp.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include "include/core/SkCanvas.h"
+#include "include/core/SkData.h"
+#include "include/core/SkStream.h"
+#include "include/core/SkSurface.h"
+#include "tools/flags/CommandLineFlags.h"
+#include "tools/trace/EventTracingPriv.h"
+#include "include/core/SkPicture.h"
+#include "include/core/SkPictureRecorder.h"
+#include "tools/Resources.h"
+#include "tools/debugger/DebugCanvas.h"
+
+using namespace std;
+
+static DEFINE_string(dir, "skia_opt_research/skps", "directory where to output skps");
+
+void raster(int width, int height, void (*draw)(SkCanvas*), const char* dir, const char *testcase_name) {
+    SkPictureRecorder recorder;
+    SkCanvas* pictureCanvas = recorder.beginRecording({0, 0, SkScalar(width), SkScalar(height)});
+    draw(pictureCanvas);
+
+    sk_sp<SkPicture> picture = recorder.finishRecordingAsPicture();
+    sk_sp<SkData> skData = picture->serialize();
+    std::string skp_path(dir);
+    skp_path.append("/");
+    skp_path.append(testcase_name);
+    SkFILEWStream skpOut(skp_path.c_str());
+    (void)skpOut.write(skData->data(), skData->size());
+}
+
+void draw_000_simpleDraw(SkCanvas *canvas) {
+    SkPaint paint;
+    paint.setColor(SK_ColorRED);
+    paint.setStrokeWidth(10);
+
+    canvas->drawLine(20, 20, 100, 100, paint);
+}
+
+void draw_001_saveLayerRect(SkCanvas *canvas) {
+    SkPaint pRed;
+    pRed.setColor(SK_ColorRED);
+
+    SkPaint pSolidBlue;
+    pSolidBlue.setColor(SK_ColorBLUE);
+
+    SkPaint pThirtyBlue;
+    pThirtyBlue.setColor(SK_ColorBLUE);
+    pThirtyBlue.setAlphaf(0.3);
+
+
+    canvas->drawRect(SkRect::MakeLTRB(10, 70, 60, 120), pRed);
+    canvas->drawRect(SkRect::MakeLTRB(150, 70, 200, 120), pRed);
+
+    canvas->saveLayer(nullptr, nullptr);
+
+    canvas->drawRect(SkRect::MakeLTRB(30, 70, 80, 120), pSolidBlue);
+    canvas->drawRect(SkRect::MakeLTRB(170, 70, 220, 120), pThirtyBlue);
+
+    canvas->restore();
+}
+
+
+int main(int argc, char **argv) {
+    CommandLineFlags::Parse(argc, argv);
+    initializeEventTracingForTools();
+
+    raster(512, 512, draw_000_simpleDraw, FLAGS_dir[0], "000_simpleDraw.skp");
+    raster(512, 512, draw_001_saveLayerRect, FLAGS_dir[0], "001_saveLayerRect.skp");
+}
