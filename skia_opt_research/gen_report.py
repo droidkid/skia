86506fc119cd38c5099b1095263aa56fb8ade114
Yuvaraj <yuvaraj.chesetti@gmail.com>
2023-02-12 23:05:17 +0000

    Percentage improvement and Summary in report

--- skia_opt_research/gen_report.py
+++ skia_opt_research/gen_report.py
@@ -33,8 +33,21 @@ proto_file.close()
 benchmark = SkiaOptMetrics.SkiaOptBenchmark()
 benchmark.ParseFromString(proto_data)
 
-opts = ['NO_OPT', 'SKIA_RECORD_OPTS', 'SKIA_RECORD_OPTS_2', 'SKI_PASS']
+opts = ['NO_OPT', 'SKIA_RECORD_OPTS', 'SKIA_RECORD_OPTS_2']
 skp_membench_results = []
+
+summary_result = {}
+summary_result['name'] = 'SUMMARY'
+summary_result['json'] = ('#')
+summary_result['ref_img_url'] = ('#')
+summary_result['skipass_log'] = ('#')
+summary_result['skipass_value'] = 0
+summary_result['skipass_link'] = '#'
+for opt in opts:
+    summary_result[opt] = {}
+    summary_result[opt]['value'] = 0
+    summary_result[opt]['link'] = '#'
+
 for skp_benchmark in benchmark.skp_benchmark_runs:
     skp_name = os.path.basename(skp_benchmark.skp_name)
 
@@ -43,12 +56,44 @@ for skp_benchmark in benchmark.skp_benchmark_runs:
     skp_membench_result['json'] = ('json/%s.json' % skp_name)
     skp_membench_result['ref_img_url'] = ("renders/%s.png" % skp_name)
     skp_membench_result['skipass_log'] = ('./%s_SKI_PASS_SkiPassRunResult.txt' % (skp_name))
+
+    # First extract SKI_PASS results.
     for opt_benchmark in skp_benchmark.optimization_benchmark_runs:
         opt = SkiaOptMetrics.Optimization.Name(opt_benchmark.optimization_type)
+        if opt != 'SKI_PASS':
+            continue
+        skp_membench_result['skipass_value'] = opt_benchmark.malloc_allocated_bytes
+        skp_membench_result['skipass_link'] = ('./%s_%s_log.txt' % (skp_membench_result['name'], opt)) 
+
+        skipass_mem = int(opt_benchmark.malloc_allocated_bytes)
+        summary_result['skipass_value'] += skipass_mem
+
+    for opt_benchmark in skp_benchmark.optimization_benchmark_runs:
+        opt = SkiaOptMetrics.Optimization.Name(opt_benchmark.optimization_type)
+        if opt == 'SKI_PASS':
+            continue
         skp_membench_result[opt] = {}
         skp_membench_result[opt]['value'] = opt_benchmark.malloc_allocated_bytes
         skp_membench_result[opt]['link'] = ('./%s_%s_log.txt' % (skp_membench_result['name'], opt)) 
+
+        mem = int(opt_benchmark.malloc_allocated_bytes)
+        if mem != 0: 
+            skp_membench_result[opt]['comp'] = "{:.1f}".format(100.0 * (mem-skipass_mem)/mem)
+        elif skipass_mem == 0:
+            skp_membench_result[opt]['comp'] = 0.0
+        else:
+            skp_membench_result[opt]['comp'] = 'WTF, we made it worse!'
+        summary_result[opt]['value'] += mem
+
+
     skp_membench_results.append(skp_membench_result)
+
+for opt in opts:
+    mem = summary_result[opt]['value']
+    skipass_mem = summary_result['skipass_value']
+    summary_result[opt]['comp'] = "{:.1f}".format(100.0 * (mem-skipass_mem)/mem)
+
+skp_membench_results.append(summary_result)
 
 template_loader = jinja2.FileSystemLoader(searchpath = "/")
 template_env = jinja2.Environment( loader=template_loader)
